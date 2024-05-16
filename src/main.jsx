a3d200763007c4dbabe3ab66cce9da86985cb736
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-05-16 18:55:20 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { MuiThemeContextProvider } from './context/MuiThemeContext'
+import { ThemeContextProvider } from './context/ThemeContext'
+import { RouteContextProvider } from './context/RouteContext';
+import{ BrowserRouter } from "react-router-dom";
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <RouteContextProvider>
+                <MuiThemeContextProvider>
+                    <ThemeContextProvider>
+                        <App/>
+                    </ThemeContextProvider>
+                </MuiThemeContextProvider>
+            </RouteContextProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
 )
