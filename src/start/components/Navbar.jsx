e3140c2117017a01b4eec335b94efcbbe7d3a640
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-05-21 14:48:40 -0400

    Remove code commented

--- src/start/components/Navbar.jsx
+++ src/start/components/Navbar.jsx
@@ -48,34 +48,6 @@ function Navbar({ current }) {
         }))
     }, [current]);
 
-    /* <nav className={"sticky top-0 z-50" + theme.navbarBg}>
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div className="relative flex h-16 items-center justify-between">
-                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        {/* <button type="button" className={"inline-flex items-center justify-center rounded-md p-2 focus:outline-none" + theme.hoverNavbar} aria-controls="mobile-menu" aria-expanded="false" onClick={toggleCollapse}>
-                        <span className="sr-only">Open main menu</span>
-                        <MenuIcon className={theme.textNavbar} onClick={showMenu}/>
-                        </button> *}
-                        </div>
-                        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                            
-                            //Logo
-                            
-                            
-                            </div>
-                        </div>
-                        {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                            <button onClick={toggleTheme} className={"rounded-full p-2" + theme.hoverNavbar}>
-                                {theme.buttonIcon}
-                            </button>
-                        </div> /}
-                    </div>
-                </div>
-            
-                {/* <MenuItems showMenu={showMenu} active={active}/> *}
-            </nav>
-         */
-
     return (
         <nav className={"sticky top-0 z-50 border-gray-200" + theme.navbarBg}>
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
