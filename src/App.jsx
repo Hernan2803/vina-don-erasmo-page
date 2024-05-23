719ca778064ec9cc986116d43e54dd8c83c6e7d6
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-05-23 18:43:07 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { useContext, useState } from 'react'
 import { RouteContext } from './context/RouteContext'
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout as Start } from './start/Layout'; 
 
 function App() {
     const { allRoutes } = useContext(RouteContext);
 
     return (
-        <Routes>
-            {allRoutes.map((page, idx) => {
-                if (page.url === "/" || page.url === "/docs") {
-                    return <Route key={`route-${idx}-to-${page}`} path={page.url} element={page.comp}/>;
-                } else {
-                    return <Route key={`route-${idx}-to-${page}`} path={page.url} element={logged ? page.comp : <Navigate replace to={"/"}/>}/>;
-                }
-            })}
-        </Routes>
+        <Start/>
     )
 }
 
