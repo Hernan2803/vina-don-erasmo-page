40eb61c6f5b1e1c9f3aff1e4ffa01a58e5b4035d
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-05-23 17:47:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/vina-don-erasmo-page/',
+  server: {
+    headers: {
+      'Permissions-Policy': 'interest-cohort=()'
+    }
+  }
 })
