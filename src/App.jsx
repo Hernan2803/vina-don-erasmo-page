331f6c5b739d66e916ed7cb25a0e72580fa791d5
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-05-23 18:57:06 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useContext, useState } from 'react'
-import { RouteContext } from './context/RouteContext'
-import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout as Start } from './start/Layout'; 
 
 function App() {
-    const { allRoutes } = useContext(RouteContext);
 
     return (
         <Start/>
