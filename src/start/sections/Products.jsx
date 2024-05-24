1dde43c5293ff38a34d398d371c426e2f6c1273c
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-05-24 02:01:11 -0400

    Update Products.jsx

--- src/start/sections/Products.jsx
+++ src/start/sections/Products.jsx
@@ -1,14 +1,15 @@
 import { ThemeContext } from '../../context/ThemeContext'
 import { useContext } from 'react'
-import ProductsMenu from '../components/ProductsMenu';
+import ProductsMenu from '../components/ProductsMenu'
 
-function Products({ current }) {
+function Products() {
 
-    const { theme } = useContext(ThemeContext);
+    const { theme } = useContext(ThemeContext)
 
     return (
-        <section className={theme.mainBg} id="products">
-            <div className="md:container md:mx-auto">
+        <section className={"pt-20 pb-12 lg:pt-[120px] lg:pb-[90px]" + theme.mainBg} id="products">
+            <h2 className={theme.sectionText}>Productos</h2>
+            <div className="container mx-auto">
                 <ProductsMenu/>
             </div>
         </section>
