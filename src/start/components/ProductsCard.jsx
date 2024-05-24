700b84dc24cf60daf815a838e73f23a6fa138228
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-05-24 00:19:30 -0400

    Update ProductsCard.jsx

--- src/start/components/ProductsCard.jsx
+++ src/start/components/ProductsCard.jsx
@@ -10,7 +10,7 @@ function ProductsCard({product}) {
     return (
         <div>
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
+                <img className="w-full" src="" alt="Sunset in the mountains"/>
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">{product.translations.sp.name}</div>
                     <p className="text-gray-700 text-base">
