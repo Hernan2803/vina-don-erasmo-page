b24b08ba71c9794516a14c9c57e3c42be824b3d4
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-05-24 01:16:05 -0400

    Fic className buttons and fix id from cards

--- src/start/components/ProductsMenu.jsx
+++ src/start/components/ProductsMenu.jsx
@@ -1,9 +1,9 @@
 import { useContext, useState, useEffect } from 'react'
 import { ThemeContext } from '../../context/ThemeContext'
-import WineBarIcon from '@mui/icons-material/WineBar';
-import LiquorIcon from '@mui/icons-material/Liquor';
+import WineBarIcon from '@mui/icons-material/WineBar'
+import LiquorIcon from '@mui/icons-material/Liquor'
 import products from './../context/ProductsContext.json'
-import ProductsCard from './ProductsCard';
+import ProductsCard from './ProductsCard'
 
 function ProductsMenu() {
     const { theme } = useContext(ThemeContext);
@@ -18,16 +18,16 @@ function ProductsMenu() {
                     <div className="flex">
                         {/* <!-- Sección Izquierda --> */}
                         <div className="w-1/4 p-4 border-r">
-                            <button className={select === "wine" ? "w-full py-2 mb-2 rounded" + theme.primaryText : "w-full py-2 mb-2 rounded" + theme.mainText} onClick={() => setSelect("wine")}><WineBarIcon/>Vinos</button>
-                            <button className={select === "liquor" ? "w-full py-2 mb-2 rounded" + theme.primaryText : "w-full py-2 mb-2 rounded" + theme.mainText} onClick={() => setSelect("liquor")}><LiquorIcon/>Licores</button>
+                            <button className={"w-full py-2 mb-2 rounded" + (select === "wine" ? theme.primaryText : theme.mainText)} onClick={() => setSelect("wine")}><WineBarIcon/>Vinos</button>
+                            <button className={"w-full py-2 mb-2 rounded" + (select === "liquor" ? theme.primaryText : theme.mainText)} onClick={() => setSelect("liquor")}><LiquorIcon/>Licores</button>
                         </div>
                         {/* <!-- Sección Derecha --> */}
-                        <div id="content" className="w-3/4 p-4">
+                        <div className="w-3/4 p-4">
                             <h2 className="text-xl font-bold mb-2">{"Catalogo de " + (select === "wine" ? "Vinos" : "Licores")}</h2>
-                            <div className="grid grid-cols-3 gap-4">
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                                 {products.map(productInfo => (
                                     productInfo.category === select ? (
-                                        <ProductsCard product={productInfo} />
+                                        <ProductsCard key={"productCard" + productInfo.sku} product={productInfo} />
                                     ) : null
                                 ))}
                             </div>
