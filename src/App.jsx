6ef288809cc51c90dc4b3262175799888aa486db
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-06-06 02:13:21 -0400

    Delete Layout.jsx and edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import { useContext, useState } from 'react'
-import { Layout as Start } from './start/Layout'
+import { useContext, useRef, useState } from 'react'
+import { ThemeContext } from './context/ThemeContext'
+import Navbar from './components/Navbar'
+import Home from './sections/Home'
+import Products from './sections/Products'
+import About from './sections/About'
+import Blog from './sections/Blog'
+import Footer from './components/Footer'
 
 function App() {
+    const { theme } = useContext(ThemeContext);
+    const [ page, setPage ] = useState(0);
+    const body = useRef();
+    
+    window.onhashchange = function () {
+        history.pushState(null, "", window.location.pathname);
+    }
+
+    const scrollHandler = () => {
+        let mh = window.screen.height;
+        let st = body.current.scrollTop + mh / 1.5;
+        let screen = Math.floor(st / mh);
+        if (screen != page) {
+        setPage(screen);
+        }
+    }
+
     return (
-        <Start/>
+        <div ref={body} className={"h-screen overflow-y-scroll" + theme.scrollbar} onScroll={scrollHandler}>
+            <Navbar current={page}/>
+            <Home/>
+            <Products/>
+            <About/>
+            <Blog/>
+            <Footer/>
+        </div>
     )
 }
 
