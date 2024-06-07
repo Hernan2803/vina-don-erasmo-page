692c6a6190127d79abb817cfd7337497170007fb
Hernán Moreno <hernan.moreno2001@gmail.com>
2024-06-06 20:18:45 -0400

    Change sku to id

--- src/components/ProductsMenu.jsx
+++ src/components/ProductsMenu.jsx
@@ -25,7 +25,7 @@ function ProductsMenu() {
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                             {products.map(productInfo => (
                                 productInfo.category === select ? (
-                                    <ProductsCard key={"productCard" + productInfo.sku} product={productInfo} />
+                                    <ProductsCard key={"productCard" + productInfo.id} product={productInfo} />
                                 ) : null
                             ))}
                         </div>
