ef561f0f2d0fa19f254ea309c9ba99590a4271fc
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-07-29 18:04:12 -0400

    Sort imports and fix CardMedia and CardActions

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,17 +3,7 @@ import PropTypes from 'prop-types';
 import { ThemeContext } from './../context/ThemeContext';
 import { red } from '@mui/material/colors';
 import { styled } from '@mui/material/styles';
-import Avatar from '@mui/material/Avatar';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
+import {Avatar, Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, IconButton, Typography} from '@mui/material';
 import ZoomOutMapIcon from '@mui/icons-material/ZoomOutMap';
 import blogTag from './../context/BlogTagContext.jsx';
 
@@ -27,25 +17,6 @@ const ExpandMore = styled((props) => {
         duration: theme.transitions.duration.shortest,
     }),
 }));
-
-function convertirFecha(fechaStr) {
-    // Parse the date in YYYYMMDD format
-    const year = fechaStr.substring(0, 4);
-    const month = fechaStr.substring(4, 6);
-    const day = fechaStr.substring(6, 8);
-
-    // Create a Date object
-    const date = new Date(year, month - 1, day);
-
-    // Get the name of the month in Spanish
-    const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-    const nameMonth = months[date.getMonth()];
-
-    // Format the date to 'Month DD, YYYY'
-    const dateFormatted = `${nameMonth} ${day}, ${year}`;
-
-    return dateFormatted;
-}
 
 function BlogCard({publication}) {
 
@@ -62,11 +33,6 @@ function BlogCard({publication}) {
         const item = blogTagMenu.find(d => d.id === id);
         return item ? item.icon : '';
     };
-    const getNameTag = (id) => {
-        const item = blogTagMenu.find(d => d.id === id);
-        return item ? item.name : '';
-    };
-
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
@@ -81,28 +47,16 @@ function BlogCard({publication}) {
                     </Avatar>
                 }
                 title={publication.title}
-                subheader={convertirFecha(publication.id.slice(0, 8))}
+                subheader={getDateConverted(publication.id.slice(0, 8))}
             />
-            <CardMedia component='img'
-                height='194'
-                image={publication.image}
-                alt={publication.title + ' image'}
-            />
+            <CardMedia component='img' height='194' image={publication.image} alt={publication.title + ' image'}/>
             <CardContent>
                 <Typography variant='body2' color='text.secondary'>
                 {publication.description}
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label='Me gusta'>
-                    <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label='Compsrtir'>
-                    <ShareIcon/>
-                </IconButton>
-                <ExpandMore expand={expanded} onClick={() => handleExpandClick()} aria-label='show more'>
-                    <ZoomOutMapIcon />
-                </ExpandMore>
+                <ExpandMore expand={expanded} onClick={() => handleExpandClick()} aria-label='show more'><ZoomOutMapIcon/></ExpandMore>
             </CardActions>
             <Collapse in={expanded} timeout='auto' unmountOnExit>
                 <CardContent>
