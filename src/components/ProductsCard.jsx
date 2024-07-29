16da0d089c523bca293af16151182c8a868c5975
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-07-29 18:07:35 -0400

    Replace Collapse with ExpandedInfo and add propTypes

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react'
 import { ThemeContext } from './../context/ThemeContext'
+import PropTypes from 'prop-types';
+import MarkdownRenderer from './../utils/MarkdownRenderer';
 
 function ProductsCard({product}) {
 
@@ -12,7 +14,7 @@ function ProductsCard({product}) {
                 <div className="px-6 py-4 w-3/4">
                     <div className="font-bold text-xl mb-2">{product.translations.sp.name}</div>
                     <p className="text-gray-700 text-base">
-                    {product.translations.sp.description}
+                        <MarkdownRenderer markdown={product.translations.sp.description}/>
                     </p>
                 </div>
             </div>
@@ -20,4 +22,21 @@ function ProductsCard({product}) {
     )
 }
 
-export default ProductsCard
+ProductsCard.propTypes = {
+    product: PropTypes.shape({
+        category: PropTypes.string,
+        id: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.number
+        ]).isRequired,
+        image: PropTypes.string,
+        translations: PropTypes.shape({
+            sp: PropTypes.shape({
+                description: PropTypes.string,
+                name: PropTypes.string,
+            }),
+        }),
+    }).isRequired,
+};
+
+export default ProductsCard;
