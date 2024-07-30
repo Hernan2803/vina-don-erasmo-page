92491b9a5c8c921256e40d28feb0bd93d4abb560
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-07-30 00:10:10 -0400

    Update MenuItems.jsx

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { useContext } from "react"
 import CloseIcon from '@mui/icons-material/Close';
 import { ThemeContext } from "./../context/ThemeContext"
 
 const MenuItems = ({showMenu, active}) => {
   const { theme } = useContext(ThemeContext);
+
   return (
-    <div onClose={showMenu}>
+    <div>
       <ul className={active ? 'flex-col flex items-center fixed inset-0 right-1/4 uppercase backdrop-blur-sm gap-8 p-8 md:hidden' + theme.primaryText + theme.softBg : 'hidden'}>
         <button onClick={showMenu}><CloseIcon/></button>
         <li><a href="#home" onClick={showMenu} className="text-center hover:underline">Inicio</a></li>
@@ -16,6 +16,6 @@ const MenuItems = ({showMenu, active}) => {
       </ul>
     </div>
   );
-};
+}
 
 export default MenuItems;
