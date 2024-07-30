88c4fb50956485dba2680779092e36cc9bcef6f2
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-07-30 00:12:08 -0400

    Update theme

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -8,18 +8,18 @@ function ProductsCard({product}) {
     const { theme } = useContext(ThemeContext)
 
     return (
-        <div className="max-w-sm bg-main-purple-300 shadow-md rounded-lg overflow-hidden shadow-lg">
-            <div className="flex">
-                <img className="h-full w-1/4" src="" alt={product.image}/>
-                <div className="px-6 py-4 w-3/4">
-                    <div className="font-bold text-xl mb-2">{product.translations.sp.name}</div>
-                    <p className="text-gray-700 text-base">
+        <div className={'max-w-sm shadow-md rounded-lg overflow-hidden shadow-lg' + theme.cardBg}>
+            <div className='flex'>
+                <img className='h-full w-1/4' src='' alt={product.image}/>
+                <div className='px-6 py-4 w-3/4'>
+                    <div className={'font-bold text-xl mb-2' + theme.cardText}>{product.translations.sp.name}</div>
+                    <p className={'text-base' + theme.cardText}>
                         <MarkdownRenderer markdown={product.translations.sp.description}/>
                     </p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 ProductsCard.propTypes = {
