d639a56e4cf897809b00d6290997cf62629a9ced
Hernan Moreno <hernan.moreno2001@gmail.com>
2024-07-30 00:12:44 -0400

    Update theme and add category 'all'

--- src/components/ProductsMenu.jsx
+++ src/components/ProductsMenu.jsx
@@ -1,17 +1,17 @@
 import { useContext, useState, useEffect } from 'react'
 import { ThemeContext } from './../context/ThemeContext'
+import GradeIcon from '@mui/icons-material/Grade';
 import products from './../context/ProductsContext.json'
 import ProductsCard from './ProductsCard'
-import productsCategory from './../context/ProductsCategoryContext.jsx'
+import productsCategory from './../context/ProductsCategoryContext'
 
 function ProductsMenu() {
     const { theme } = useContext(ThemeContext);
-    const [select, setSelect] = useState('');
+    const [select, setSelect] = useState('all');
     const [productsCategoryMenu, setProductsCategoryMenu] = useState([]);
 
     useEffect(() => {
         const productsCategoryMenu = productsCategory();
-        setSelect(productsCategoryMenu[0].id);
         setProductsCategoryMenu(productsCategoryMenu);
     }, []);
 
@@ -21,23 +21,25 @@ function ProductsMenu() {
     };
 
     return (
-        <div className="flex items-center justify-center max-h-screen bg-gray-100">
-
-            <div className="w-full bg-white shadow-md rounded-lg overflow-hidden ">
-                <div className="flex">
+        <div className='flex items-center justify-center max-h-screen bg-gray-100'>
+            <div className='w-full bg-white shadow-md rounded-lg overflow-hidden'>
+                <div className='flex'>
                     {/* <!-- Sección Izquierda --> */}
-                    <div className="w-1/4 p-4 border-r">
+                    <div className='w-1/4 p-4 border-r'>
+                        <button key={'all'} className={'w-full py-2 mb-2 rounded' + (select === 'all' ? theme.highText : theme.mainText)} onClick={() => setSelect('all')}><GradeIcon/>{'Todos'}</button>
                         {productsCategoryMenu.map(element => (
-                            <button key={element.id} className={"w-full py-2 mb-2 rounded" + (select === element.id ? theme.primaryText : theme.mainText)} onClick={() => setSelect(element.id)}>{element.icon}{element.name}</button>
+                            <button key={element.id} className={'w-full py-2 mb-2 rounded' + (select === element.id ? theme.highText : theme.mainText)} onClick={() => setSelect(element.id)}>{element.icon}{element.name}</button>
                         ))}
                     </div>
                     {/* <!-- Sección Derecha --> */}
-                    <div className="w-3/4 p-4">
-                        <h2 className="text-xl font-bold mb-2">{"Catalogo de " + getNameSelected()}</h2>
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div className='w-3/4 p-4'>
+                        <h2 className={'text-xl font-bold mb-2' + theme.titleText}>{'Catalogo de ' + getNameSelected()}</h2>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                             {products.map(productInfo => (
-                                productInfo.category === select ? (
-                                    <ProductsCard key={"productCard" + productInfo.id} product={productInfo} />
+                                select === 'all' ? (
+                                    <ProductsCard key={'productCard' + productInfo.id} product={productInfo} />
+                                ) : productInfo.category === select ? (
+                                    <ProductsCard key={'productCard' + productInfo.id} product={productInfo} />
                                 ) : null
                             ))}
                         </div>
@@ -45,7 +47,7 @@ function ProductsMenu() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default ProductsMenu
+export default ProductsMenu;
